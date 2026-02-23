a9fe75a00132dacab90418f476efc13d9175d4d5
Heni Patel <henipatel8913@gmail.com>
2026-02-23 15:25:59 -0500

    Add app constants and configuration values

---
+++ app/src/main/kotlin/com/bookaholic/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.bookaholic.util
+
+object Constants {
+    const val BASE_URL = "https://api.bookaholic.com/"
+    const val TIMEOUT_DURATION = 30L // in seconds
+    const val MAX_BOOKS_PER_PAGE = 20
+    // Add any additional constants here
+}
